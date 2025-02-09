5874ad7ee93e75e5f7b3cce5b1d80ac6ecf78ca3
Enrique Cametti <quiquecametti97@gmail.com>
2025-02-09 17:51:59 -0300

    Added Bazarr
    Added Bazarr to torrent stack compose file and ensure_basepaths.sh
    script

--- torrent/ensure_basepaths.sh
+++ torrent/ensure_basepaths.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
 
-for svc in {prowlarr,radarr,readarr,sonarr};do
-    docker compose exec -ti  ${svc} sed -i "s|<UrlBase></UrlBase>|<UrlBase>/${svc}</UrlBase>|g" /config/config.xml
+for svc in {bazarr,prowlarr,radarr,readarr,sonarr}; do
+  docker compose exec -ti ${svc} sed -i "s|<UrlBase></UrlBase>|<UrlBase>/${svc}</UrlBase>|g" /config/config.xml
 done
 
 docker compose restart prowlarr radarr readarr sonarr
+
