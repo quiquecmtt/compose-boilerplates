1b5ac27854397ba405e10db6b36c1c6d91536e75
Enrique Cametti <44720459+quiquecmtt@users.noreply.github.com>
2025-11-26 18:46:04 -0300

    Don't do this any more Quique
    Multiple changes

--- torrent/post-scripts/ensure_basepaths.sh
+++ torrent/post-scripts/ensure_basepaths.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
-for svc in {prowlarr,radarr,readarr,sonarr}; do
+for svc in {prowlarr,radarr,sonarr}; do
   docker compose exec -ti ${svc} sed -i "s|<UrlBase></UrlBase>|<UrlBase>/${svc}</UrlBase>|g" /config/config.xml
 done
 
 docker compose exec -ti bazarr sed -i "s|base_url: ''|base_url: /bazarr|g" /config/config/config.yaml
 
-docker compose restart prowlarr radarr readarr sonarr
+docker compose restart prowlarr radarr sonarr bazarr
